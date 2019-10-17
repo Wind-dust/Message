ad3c32af9480f847ad9c86c2032b3345543e1d52
wind-dust <1002380377@qq.com>
2019-10-17 16:40:30 +0800

    修正接口查询代码

--- application/index/controller/ApplicationCase.php
+++ application/index/controller/ApplicationCase.php
@@ -13,7 +13,7 @@ class ApplicationCase extends MyController {
      * @apiParam (入参) {String} page 页码
      * @apiParam (入参) {String} pageNum 条数
      * @apiSuccess (返回) {String} code 200:成功 / 3000:未获取到数据 / 3002.type参数错误 / 3003.pid参数错误
-     * @apiSampleRequest /index/applicationCase/getApplicationCase
+     * @apiSampleRequest /index/applicationcase/getApplicationCase
      * @author rzc
      */
     public function getApplicationCase() {
@@ -23,7 +23,7 @@ class ApplicationCase extends MyController {
         $pageNum    = trim($this->request->post('pageNum'));
         $page       = is_numeric($page) ? $page : 1;
         $pageNum    = is_numeric($pageNum) ? $pageNum : 10;
-        $result   = $this->app->applicationCase->getApplicationCase($page, $pageNum,$applicationCase_id);
+        $result   = $this->app->applicationcase->getApplicationCase($page, $pageNum,$applicationCase_id);
         // $this->apiLog($apiName, [$applicationCase_id, $source], $result['code'], '');
         return $result;
     }
--- application/index/controller/DownloadCenter.php
+++ application/index/controller/DownloadCenter.php
@@ -13,7 +13,7 @@ class DownloadCenter extends MyController {
      * @apiParam (入参) {String} page 页码
      * @apiParam (入参) {String} pageNum 条数
      * @apiSuccess (返回) {String} code 200:成功 / 3000:未获取到数据 / 3002.type参数错误 / 3003.pid参数错误
-     * @apiSampleRequest /index/downloadCenter/getDownloadCenter
+     * @apiSampleRequest /index/downloadcenter/getDownloadCenter
      * @author rzc
      */
     public function getDownloadCenter() {
@@ -23,7 +23,7 @@ class DownloadCenter extends MyController {
         $pageNum    = trim($this->request->post('pageNum'));
         $page       = is_numeric($page) ? $page : 1;
         $pageNum    = is_numeric($pageNum) ? $pageNum : 10;
-        $result   = $this->app->downloadCenter->getDownloadCenter($page, $pageNum,$downloadCenter_id);
+        $result   = $this->app->downloadcenter->getDownloadCenter($page, $pageNum,$downloadCenter_id);
         // $this->apiLog($apiName, [$downloadCenter_id, $source], $result['code'], '');
         return $result;
     }
